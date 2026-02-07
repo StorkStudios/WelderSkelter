daefe58e121bc888d159a53cf0149483bb8ed0bf
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2026-02-07 20:35:12 +0100

    bulech

--- Assets/Scripts/PlayerUpgrades/Upgrades/Bulech/LessItemsMoreMoneyUpgrade.cs
+++ Assets/Scripts/PlayerUpgrades/Upgrades/Bulech/LessItemsMoreMoneyUpgrade.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "LessItemsMoreMoneyUpgrade", menuName = "Scriptable Objects/LessItemsMoreMoneyUpgrade")]
-public class LessItemsMoreMoneyUpgrade : Upgrade, IUpgrade<MoneyManager.MoneyManagerModifiers>
+namespace BulechUpgrades
 {
-    [SerializeField]
-    private float incomeMultiplier;
-    [SerializeField]
-    private float perItemIncomeMultiplierIncrement;
-
-    public void ApplyModifier(MoneyManager.MoneyManagerModifiers modifier)
+    [CreateAssetMenu(fileName = "LessItemsMoreMoneyUpgrade", menuName = "Scriptable Objects/LessItemsMoreMoneyUpgrade")]
+    public class LessItemsMoreMoneyUpgrade : Upgrade, IUpgrade<MoneyManager.MoneyManagerModifiers>
     {
-        modifier.allIncomeMultipler *= incomeMultiplier;
-        modifier.perItemIncomeMultiplier += perItemIncomeMultiplierIncrement;
+        [SerializeField]
+        private float incomeMultiplier;
+        [SerializeField]
+        private float perItemIncomeMultiplierIncrement;
+
+        public void ApplyModifier(MoneyManager.MoneyManagerModifiers modifier)
+        {
+            modifier.allIncomeMultipler *= incomeMultiplier;
+            modifier.perItemIncomeMultiplier += perItemIncomeMultiplierIncrement;
+        }
     }
 }
