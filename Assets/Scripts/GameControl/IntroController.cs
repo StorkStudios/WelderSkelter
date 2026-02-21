4be2d891920757c6cc1c3f32f456642ea165f401
Gahlammon <adam@dachtera.pl>
2026-02-21 17:15:08 +0100

    Create a terrible script that turns on the tutorial after watching the cutscene

--- Assets/Scripts/GameControl/IntroController.cs
+++ Assets/Scripts/GameControl/IntroController.cs
@@ -7,12 +7,25 @@ using SceneEnum = StorkStudios.CoreNest.Scene;
 public class IntroController : MonoBehaviour
 {
     public bool LoadMainScene = false;
+    public bool DontLoadMainScene = false;
 
     private void Update()
     {
-        if (LoadMainScene || Input.GetKey(KeyCode.Escape))
+        if (LoadMainScene && DontLoadMainScene)
+        {
+            MainMenuController.Instance.StartTutorial();
+            SceneManager.UnloadSceneAsync(SceneEnum.Intro.GetBuildIndex());
+        }
+        if (LoadMainScene && !DontLoadMainScene)
+        {
+            DontLoadMainScene = true;
+            SceneManager.LoadScene(SceneEnum.SampleScene.GetBuildIndex(), LoadSceneMode.Additive);
+        }
+        if (Input.GetKey(KeyCode.Escape))
         {
             SceneManager.LoadScene(SceneEnum.SampleScene.GetBuildIndex(), LoadSceneMode.Single);
         }
+
+        //I regret nothing.
     }
 }
